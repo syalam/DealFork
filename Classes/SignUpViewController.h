4418fef80af2cd70f477a178b68df70536889efd
syalam <sheehan@stocktwits.com>
2011-03-13 12:07:48 +0000

    added signup view

---
+++ Classes/SignUpViewController.h
@@ -0,0 +1,16 @@
+//
+//  SignUpViewController.h
+//  DealFork
+//
+//  Created by Sheehan Alam on 3/13/11.
+//  Copyright 2011 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SignUpViewController : UIViewController {
+    
+}
+
+@end
